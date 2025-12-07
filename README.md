e233474ed2857d9e784e8b197d06a25dffc1a4b3
AlexVaizer <wiseralex@gmail.com>
2025-12-07 21:33:09 +0200

    readme added

---
+++ README.md
@@ -0,0 +1,15 @@
+# Main Features:
+- Backs up Softwares on Ubuntu distros
+- Generates Bash script to restore(2) them back
+All of this configured via JSON Config File
+
+Accepts Config file path as a first argument ("./backup_config.json" used if no arguments sent)
+
+By default runs in NOOP mode (does not copy files, just generate output)
+If you want it to actually backup things:
+- send 'true' as a second argument
+- must be run with sudo
+_Example: `sudo ruby backup.rb ~/backups/configs/servername.example.com.json true`_
+
+(1) JSON Config File data model check in vm-configs/\_template.json
+(2) restore.sh example see in 'backup.rb.output.example' file
--- backup.rb
+++ backup.rb
@@ -1,19 +1,4 @@
 #!/usr/bin/ruby
-#####################
-### Main Features:
-###   1. Backs up Softwares on Ubuntu distros
-###   2. Generates Bash script to restore(2) them back
-### All of this configured via JSON Config File
-### Accepts Config file path as a first argument ("./backup_config.json" used if no arguments sent)
-### By default runs in NOOP mode (does not copy files, just generate output)
-### If you want it to actually backup things:
-###   - send 'true' as a second argument,
-###   - And Must be run with sudo
-### Example: 'sudo ruby backup.rb ~/backups/configs/servername.example.com.json true'
-
-### (1) JSON Config File data model check in vm-configs/_template.json
-### (2) restore.sh example see in 'backup.rb.output.example' file
-
 require "json"
 require "fileutils"
 require "erb"
