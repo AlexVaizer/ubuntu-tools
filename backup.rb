c240371bd10bd83cf255f7613b76cac3a97aa7c3
AlexVaizer <37403333+AlexVaizer@users.noreply.github.com>
2025-12-08 01:16:07 +0200

    fixed var name

--- backup.rb
+++ backup.rb
@@ -49,7 +49,7 @@ apt install -y <%=@aptPackages.join(' ')%>
 def gsubVars(string)
 	return string.gsub("$BACKUP_PATH", BACKUP_PATH)
 		.gsub("$COCKPIT_USERNAME", COCKPIT_USERNAME)
-		.gsub("$COCKPIT_PASSWORD", COCKPIT_PASSWORD)
+		.gsub("$COCKPIT_USER_PASSWORD", COCKPIT_USER_PASSWORD)
 		.gsub("$START_TIME", START_TIME)
 		.gsub("$ROOT_PATH", ROOT_PATH)
 		.gsub("$USERNAME", USERNAME)
@@ -58,7 +58,7 @@ end
 def gsubVarsRestore(string)
 	return string.gsub("$BACKUP_PATH", '.')
 		.gsub("$COCKPIT_USERNAME", COCKPIT_USERNAME)
-		.gsub("$COCKPIT_PASSWORD", COCKPIT_PASSWORD)
+		.gsub("$COCKPIT_USER_PASSWORD", COCKPIT_USER_PASSWORD)
 		.gsub("$START_TIME", START_TIME)
 		.gsub("$ROOT_PATH", ROOT_PATH)
 		.gsub("$USERNAME", USERNAME)
@@ -70,7 +70,7 @@ def doBackupCommandsAndPrepareRestoreCommands(confHash = {})
 	puts "#{H1_PREFIX} Backing up files"
 	puts "#{H1_PREFIX} (NOOP MODE IS ON, NO FILES COPYING)" if NOOP
 	puts "#{H1_PREFIX} ENTER A PASSWORD FOR COCKPIT_USER:"
-	COCKPIT_PASSWORD = gets.chomp
+	COCKPIT_USER_PASSWORD = gets.chomp
 	puts SECTIONS_SEPARATOR
 	@preCommands = []
 	@restoreCommands = []
